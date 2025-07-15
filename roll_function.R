133a3b1a82b72095e7baa8eb5b4a57398f6bf0d0
javiocu <javiocu@protonmail.com>
2025-07-15 19:10:24 +0200

    # Function roll now admits any dice with x sides.

--- roll_function.R
+++ roll_function.R
@@ -1,8 +1,8 @@
 # Función que suma la simulación de haber tirado
 # dos dados de seis caras y suma sus valores
 
-roll <- function() {
-  die <- 1:6
+roll <- function(bones=1:6) {
+  die <- bones
   dice <- sample(die, size = 2, replace = TRUE)
   sum(dice)
 }
