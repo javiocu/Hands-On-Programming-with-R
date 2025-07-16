5b9d2ea93c665b078d75f0512ec2ef5d82821235
javiocu <javiocu@protonmail.com>
2025-07-16 13:36:54 +0200

    # Incrementada la probabilidad de que salga más veces el valor de 6.

--- roll_function.R
+++ roll_function.R
@@ -3,6 +3,8 @@
 
 roll <- function(bones=1:6) {
   die <- bones
-  dice <- sample(die, size = 2, replace = TRUE)
+  dice <- sample(die, size = 2, 
+                 replace = TRUE, 
+                 prob = c(0.1,0.1,0.1,0.1,0.1,0.5))
   sum(dice)
 }
