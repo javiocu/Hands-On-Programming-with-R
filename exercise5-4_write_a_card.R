15d274ada7f38f316d2c19a0bdaea93524b44fb2
javiocu <javiocu@protonmail.com>
2025-07-16 16:34:02 +0200

    # Added exercise 5.4

---
+++ exercise5-4_write_a_card.R
@@ -0,0 +1,22 @@
+deck <- data.frame(face="ace", suit="hearts", value=1)
+
+hand = c("king","queen","jack","ten","nine","eight","seven",
+         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eight","seven",
+         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eight","seven",
+         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eight","seven",
+         "six", "five","four","three","two","one")
+
+suit = c("spades","spades","spades","spades","spades","spades",
+        "spades","spades","spades","spades","spades","spades","spades",
+        "clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs",
+        "clubs","clubs","clubs","clubs","clubs",
+        "diamonds","diamonds","diamonds","diamonds","diamonds","diamonds",
+        "diamonds","diamonds","diamonds","diamonds","diamonds","diamonds",
+        "diamonds",
+        "hearts","hearts","hearts","hearts","hearts","hearts","hearts",
+        "hearts","hearts","hearts","hearts","hearts","hearts")
+
+values = c(13:1,13:1,13:1,13:1)
+
+
+deck = data.frame(hand, suit, values)
