a373d7e78218724d56cbdea930d6941e52443592
javiocu <javiocu@protonmail.com>
2025-07-16 14:44:20 +0200

    # Adde the file for create a simple deck

---
+++ Exercise_making_a_deck.R
@@ -0,0 +1,8 @@
+# Creando el vector de las cartas
+hand <- c("ace","king","queen","jack","ten",
+          "spades","spades","spades","spades",
+          "spades","spades")
+
+deck <- matrix(hand,ncol=2,nrow=5)
+
+
