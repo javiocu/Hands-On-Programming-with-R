cced94628a1eacaa643ebaeb2dbd7b9b1b496e7f
javiocu <javiocu@protonmail.com>
2025-07-16 21:17:20 +0200

    Change the name

--- Exercise_making_a_deck.R
+++ Exercise_making_a_deck.R
@@ -3,6 +3,5 @@ hand <- c("ace","king","queen","jack","ten",
           "spades","spades","spades","spades",
           "spades","spades")
 
-deck <- matrix(hand,ncol=2,nrow=5)
-
+hand1 <- matrix(hand,ncol=2,nrow=5)
 
