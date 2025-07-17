2c6cbd8343dd228fad17f5e819bcd6abb25f4979
javiocu <javiocu@protonmail.com>
2025-07-17 13:41:16 +0200

    # Make the skeleton for the score function.

---
+++ 9-Programs/Function-score.R
@@ -0,0 +1,16 @@
+# Score function to calculate the score o the symbols obtains previously
+
+score <- function(symbols) {
+  if ( # all the same ) {
+    prize <- #look the table for the prize
+} else if (# all bars) {
+    prize <- 5$
+  } else {
+    # count cherries
+    prize <- # calculate it
+  }
+}
+
+
+# count diamonds 
+# double the prize if necessary
