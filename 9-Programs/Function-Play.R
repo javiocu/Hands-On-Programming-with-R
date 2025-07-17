c2489b3a5ec6649c0497d7584f619a6cbee8ee53
javiocu <javiocu@protonmail.com>
2025-07-17 15:43:08 +0200

    # Modified the name wheel for symbols.

--- 9-Programs/Function-Play.R
+++ 9-Programs/Function-Play.R
@@ -4,10 +4,10 @@
 play <- function() {
   get_symbols <- function() {
     symbols <- c("DD", "7", "BBB", "BB", "B", "C", "0")
-    wheel <- sample(symbols, 3, replace = TRUE,
+    symbols <- sample(symbols, 3, replace = TRUE,
                     prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52)
     )
-    print(wheel)
+    print(symbols)
   }
   list(symbols = get_symbols)
 }
