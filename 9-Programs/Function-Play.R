3a6f810e79143257b7309a29a355d157ad8e5d47
javiocu <javiocu@protonmail.com>
2025-07-17 13:36:42 +0200

    ## Added a function in a fina environment to play with casinos`s machines.

---
+++ 9-Programs/Function-Play.R
@@ -0,0 +1,13 @@
+# Function that play the whole game, taking the three symbols, 
+# calculate the prize and showing the result
+
+play <- function() {
+  get_symbols <- function() {
+    symbols <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+    wheel <- sample(symbols, 3, replace = TRUE,
+                    prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52)
+    )
+    print(wheel)
+  }
+  list(symbols = get_symbols)
+}
