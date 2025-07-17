64f1b7610e795ba80458f4241d9142986b72a975
javiocu <javiocu@protonmail.com>
2025-07-17 17:46:24 +0200

    # Corrected the file

--- exercise5-4_write_a_card.R
+++ exercise5-4_write_a_card.R
@@ -1,16 +1,10 @@
 deck <- data.frame(face="ace", suit="hearts", value=1)
 
-<<<<<<< HEAD
-hand = c("king","queen","jack","ten","nine","eight","seven",
-         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eight","seven",
-         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eight","seven",
-         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eight","seven",
-=======
 hand = c("king","queen","jack","ten","nine","eigth","seven",
-         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eigth","seven",
-         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eigth","seven",
-         "six", "five","four","three","two","one","king","queen","jack","ten","nine","eigth","seven",
->>>>>>> 25c1d528bec06e79dbc01f32b42cae11dd31141b
+         "six", "five","four","three","two","one","king","queen","jack","ten",
+         "nine","eigth","seven","six", "five","four","three","two","one","king",
+         "queen","jack","ten","nine","eigth","seven","six", "five","four",
+         "three","two","one","king","queen","jack","ten","nine","eigth","seven",
          "six", "five","four","three","two","one")
 
 suit = c("spades","spades","spades","spades","spades","spades",
@@ -19,9 +13,8 @@ suit = c("spades","spades","spades","spades","spades","spades",
         "clubs","clubs","clubs","clubs","clubs",
         "diamonds","diamonds","diamonds","diamonds","diamonds","diamonds",
         "diamonds","diamonds","diamonds","diamonds","diamonds","diamonds",
-        "diamonds",
-        "hearts","hearts","hearts","hearts","hearts","hearts","hearts",
-        "hearts","hearts","hearts","hearts","hearts","hearts")
+        "diamonds","hearts","hearts","hearts","hearts","hearts","hearts",
+        "hearts","hearts","hearts","hearts","hearts","hearts","hearts")
 
 values = c(13:1,13:1,13:1,13:1)
 
