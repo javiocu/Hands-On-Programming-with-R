640d21e08d2447c6e0cdcf9c4e9a8f4a29420261
javiocu <javiocu@protonmail.com>
2025-07-22 19:29:51 +0200

    # Updated the play function to receive the multiples symbols at once time.

--- 9-Programs/Function-Play.R
+++ 9-Programs/Function-Play.R
@@ -1,8 +1,10 @@
 # Function that play the whole game, taking the three symbols, 
 # calculate the prize and showing the result
 
-play <- function() {
-  symbols <- get_symbols()
-  prize <- structure(scoreV2(symbols), "symbols" = symbols, "class" = "slots")
-  prize
-  }
+play <- function(n) {
+  symbols <- get_symbols(n = n)
+  datass <- data.frame(w1 = symbols[,1], w2 = symbols[,2],
+             w3 = symbols[,3], prize = NA)
+  datass$prize <- apply(datass[, c("w1", "w2", "w3")], 1, scoreV2)
+  datass
+}
