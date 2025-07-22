933740ee7a40ec0f8cccd4ad2a05bcfbf6371f6e
javiocu <javiocu@protonmail.com>
2025-07-22 18:28:19 +0200

    # Included time tests

--- 12-Speed/Function_change_symbolsV2.R
+++ 12-Speed/Function_change_symbolsV2.R
@@ -1,7 +1,14 @@
 
+vec <- c("DD", "C", "7", "B", "BB", "BBB", "0")
+vec <- rep(vec, 20000000)
+
+
 change_symbolsV2 <- function(vec) {
   valor <- c("DD" = "joker", "C" = "ace", "7" = "king",
            "B" = "queen", "BB" = "jack", "BBB" = "ten", "0" = "nine")
   
   unname(valor[vec])
 }
+
+vecv2 <- system.time(change_symbolsV2(vec))
+vecv2
